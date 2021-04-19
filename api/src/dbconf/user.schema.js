e7e0608278cb23e9dc44f9a3f38ba2c6ab80d265
Andrei <andrewostin13@gmail.com>
2021-04-19 17:51:12 +0300

    added modules field to user shcema

--- api/src/dbconf/user.schema.js
+++ api/src/dbconf/user.schema.js
@@ -2,4 +2,5 @@ const mongoose = require('mongoose');
 exports.User = mongoose.model('User', {
   login: { type: String, required: true, unique: true },
   hash: { type: String, required: true },
+  modules: { type: [String], enum: ['price-parcer', 'some-other', 'none'], required: true}, 
 });
