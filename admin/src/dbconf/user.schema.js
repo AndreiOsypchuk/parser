e914a71b0de9a59336c7171315bca8bf4dcdee6e
Andrei <andrewostin13@gmail.com>
2021-04-24 00:07:30 +0300

    api is in typescript now

--- admin/src/dbconf/user.schema.js
+++ admin/src/dbconf/user.schema.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 exports.User =
   mongoose.models.User ||
   mongoose.model('User', {
-    username: { type: String, required: true, unique: true },
+    username: { type: String, unique: true, required: true },
     hash: { type: String, required: true },
-    modules: { type: [String], enum: ['parser', 'none'], default: 'none' },
-    count: { type: Number, default: 0 },
+    modules: { type: [String], required: true, default: [] },
   });
