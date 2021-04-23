cf04d60c95cf465052174f4bae50980495cae09d
Andrei <andrewostin13@gmail.com>
2021-04-23 20:28:08 +0300

    merge conflict resolution

--- api/src/dbconf/user.schema.js
+++ api/src/dbconf/user.schema.js
@@ -2,9 +2,5 @@ const mongoose = require('mongoose');
 exports.User = mongoose.model('User', {
   username: { type: String, required: true, unique: true },
   hash: { type: String, required: true },
-<<<<<<< HEAD
-  modules: { type: [String], enum: ['price-parcer', 'some-other', 'none'], required: true}, 
-=======
   modules: { type: [String], enum: ['parser', 'none'], default: 'none' },
->>>>>>> volumes
 });
